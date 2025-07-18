ce97e4a4a357c97db0fb7c44c86fed2978ebc2a7
unknown <nouraallababede@gmail.com>
2025-07-18 18:28:30 +0300

    final6

--- api/index.php
+++ api/index.php
@@ -1,14 +1,16 @@
 <?php
 
-require DIR . '/../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
-$app = require_once DIR . '/../bootstrap/app.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $request = Illuminate\Http\Request::capture();
+
 $response = $kernel->handle($request);
 
 $response->send();
 
 $kernel->terminate($request, $response);
+
